783b5414b7b7e0d7c004eb843e956f5397141b0c
andres bolivar <a.bolivarc@uniandes.edu.co>
2025-12-04 13:57:15 -0500

    Ajustes para la conexión con mongo

--- inventarios/models/db.py
+++ inventarios/models/db.py
@@ -4,13 +4,13 @@ from pydantic.functional_validators import BeforeValidator
 from typing_extensions import Annotated
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
-    f"mongodb://provesi_user:isis2503@{os.getenv('INVENTARIOS_DB_HOST', '10.128.0.86')}:27017?retryWrites=true&w=majority"
+    f"mongodb://provesi_user:isis2503@{os.getenv('INVENTARIOS_DB_HOST', '98.92.39.90')}:27017?retryWrites=true&w=majority"
 )
 db = client.get_database("provesi_db")
 inventarios_collection = db.get_collection("inventarios")
 
 
-async def set_places_db():
+async def set_inventarios_db():
     # Creates a unique index on the code field
     await inventarios_collection.create_index("codigo", unique=True)
 
