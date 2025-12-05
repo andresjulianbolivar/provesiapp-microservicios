a81d1686e27a5c4cdb0b0372292b68efd0ba57d4
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2025-12-05 05:05:46 -0500

    Refactor URL configurations for 'manejador_pedidos' app and update API endpoint names

--- manejador_pedidos/pedidos/views/urls.py
+++ manejador_pedidos/pedidos/views/urls.py
@@ -1,7 +1,15 @@
-# facturaciones/views/urls.py
+# manejador_pedidos/pedidos/views/urls.py
 from django.urls import path
-from .views import generar_factura  # desde views.py
+from .views import (
+    generar_factura,
+    crear_pedido,
+    crear_factura,
+    facturas_pendientes,
+)
 
 urlpatterns = [
-    path("generar-factura/", generar_factura, name="generar_factura"),
+    path("generar-factura/", generar_factura, name="generar_factura_api"),
+    path("crear-pedido/", crear_pedido, name="crear_pedido_api"),
+    path("crear-factura/", crear_factura, name="crear_factura_api"),
+    path("facturas-pendientes/", facturas_pendientes, name="facturas_pendientes_api"),
 ]
